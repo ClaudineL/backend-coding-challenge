ca4a8fe2484237b9fccea4164cfe516cac46ee00
Claudine Lamothe <fullywoollyherdy@gmail.com>
2019-05-29 09:18:38 -0400

    renamed query parameter

--- city_api.py
+++ city_api.py
@@ -2,12 +2,12 @@ from text_matching import text_match
 from scoring import score_results
 
 
-def get_cities(query, latitude=None, longitude=None):
-    """ Look up matching city names and return JSON list. """
+def get_cities(q, latitude=None, longitude=None):
+    """ Look up names matching query and return JSON list. """
     if latitude and longitude:
         loc = (latitude, longitude)
     else:
         loc = None
-    matches = text_match(query)
-    scored_matches = score_results(matches, query, loc)
+    matches = text_match(q)
+    scored_matches = score_results(matches, q, loc)
     return scored_matches

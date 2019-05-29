d6c04cf915a451e5da4eff1ac9538300f437d920
Claudine Lamothe <fullywoollyherdy@gmail.com>
2019-05-29 09:05:33 -0400

    changed code to use connexion and swagger

--- city_api.py
+++ city_api.py
@@ -1,27 +1,13 @@
-from flask import Flask, request, jsonify
 from text_matching import text_match
 from scoring import score_results
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Deployed to Heroku!</h1>'
-
-
-@app.route('/suggestions')
-def get_cities():
-    query = request.args['q']
-    loc = None
-    lat = request.args.get('latitude')
-    lon = request.args.get('longitude')
-    if lat and lon:
-        loc = (lat, lon)
+def get_cities(query, latitude=None, longitude=None):
+    """ Look up matching city names and return JSON list. """
+    if latitude and longitude:
+        loc = (latitude, longitude)
+    else:
+        loc = None
     matches = text_match(query)
     scored_matches = score_results(matches, query, loc)
-    return jsonify(scored_matches)
-
-
-if __name__ == '__main__':
-    app.run(debug=False)
-
+    return scored_matches
---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import render_template, request, jsonify
+import connexion
+
+app = connexion.App(__name__, specification_dir='./')
+
+app.add_api('swagger.yml')
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+# FIXME: remember to turn off!
+if __name__ == '__main__':
+    app.run(debug=True)
